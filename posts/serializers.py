3dc210212b45a145cc8851be351b90bcf6ebc8d3
StanMungai <stanfordkangethe16@gmail.com>
2024-07-28 11:31:52 +0300

    Updated views and user serializers.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +14,8 @@ class PostSerializer(serializers.ModelSerializer):
             "created_at"
         )
         model = Post
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username")
